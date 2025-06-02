495fea04d8c12a1fa84be78d4985aae87f13f6f6
Taufique Alam Ansari <taufiquealam0105@gmail.com>
2025-06-02 12:27:44 +0530

    Improved the Home PageUI, Added the STT functionality, removed navigation header

--- 01version/backend/api/v1/controllers/speechToTextController.js
+++ 01version/backend/api/v1/controllers/speechToTextController.js
@@ -13,17 +13,18 @@ export const getSpeechToText = (req, res) => {
 };
 
 export const postSpeechToText = async (req, res) => {
-  const audioBuffer = req.file?.buffer;
-
-  console.log(audioBuffer)
+  const audioBuffer = req.file.buffer;
+  const mimetype = req.file.mimetype;
+  console.log("Received audio file:", req.file.originalname, "with mimetype:", mimetype);
+  console.log("Audio buffer length:", audioBuffer.length);
   if (!audioBuffer) {
     return res.status(400).json({ error: "Missing audio file in request." });
   }
 
   const formData = new FormData();
-  const blob = new Blob([audioBuffer], { type: req.file.mimetype });
+  const blob = new Blob([audioBuffer], { type: mimetype });
 
-  formData.set('file', blob, 'audio.wav');
+  formData.set('file', blob, req.file.originalname || 'audio.wav');
   formData.set('model', 'saarika:flash');
   formData.set('language_code', 'unknown');
 
@@ -31,13 +32,14 @@ export const postSpeechToText = async (req, res) => {
     const response = await fetch('https://api.sarvam.ai/speech-to-text', {
       method: 'POST',
       headers: {
-        'api-subscription-key': 'b653a142-ba6c-4e47-b6ce-6b04f5acafcd',
-        ...formData.headers, // includes correct content-type boundary
+        'api-subscription-key': API_SECRET,
+        ...formData.headers,
       },
       body: formData
     });
 
     const data = await response.json();
+    console.log("Data in response:",data)
 
     if (!response.ok) throw new Error(data.message || 'Speech-to-text failed');
 
--- 01version/backend/api/v1/routes/speechToTextRouter.js
+++ 01version/backend/api/v1/routes/speechToTextRouter.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import speechToTextController from '../controllers/speechToTextController.js';
-import multer from 'multer';
-
+import express from "express";
+import speechToTextController from "../controllers/speechToTextController.js";
+import multer from "multer";
 
 const router = express.Router();
-const upload = multer({storage: multer.memoryStorage()});
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+});
 
-router.get('/', speechToTextController.getSpeechToText);
-router.post('/', upload.single('file'), speechToTextController.postSpeechToText);
+router.get("/", speechToTextController.getSpeechToText);
+router.post(
+  "/",
+  upload.single("audio"),
+  speechToTextController.postSpeechToText
+);
 
 export default router;
