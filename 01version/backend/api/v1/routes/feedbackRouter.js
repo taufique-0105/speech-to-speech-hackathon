0e33e8cbf63202b541d9888012a501c05a26ca8d
Taufique Alam Ansari <144208719+taufique-0105@users.noreply.github.com>
2025-06-06 19:17:03 +0530

    Update feedbackRouter.js

--- 01version/backend/api/v1/routes/feedbackRouter.js
+++ 01version/backend/api/v1/routes/feedbackRouter.js
@@ -4,6 +4,9 @@ import { getFeedback, submitFeedback } from '../controllers/feedbackControllers.
 const router = express.Router();
 
 router.post('/submit', submitFeedback);
+
+
+// Need to add a restriction for this route so everyone doesn't be able to access it as it will contain sensitive data.
 router.get('/', getFeedback);
 
 export default router;
